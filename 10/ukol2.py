60d1a4d8c682f40a1ffc73ed0317cacfab14c8bd
Dituse <you@example.com>
2024-05-25 13:05:52 +0200

    ukol2

---
+++ 10/ukol2.py
@@ -0,0 +1,28 @@
+#Úkol 2: https://github.com/andywaltlova/programovani-v-pythonu-jaro-2024/blob/main/ukoly/ukol-2-zadani.md
+#Autor: Dita Hronová
+
+import requests
+import json
+
+try:
+    response = requests.get('https://cat-fact.herokuapp.com/facts', timeout=0.001)
+    data = response.json()
+except requests.Timeout:
+    print('Jsi příliš nedočkavá/ý.')
+finally:
+    response = requests.get('https://cat-fact.herokuapp.com/facts', timeout=0.5)
+    data = response.json()
+
+
+cat_facts = []
+
+for index, item in enumerate(data[:10], start=1):
+    cat_facts.append(f"{index}. {item['text']}")
+
+print(cat_facts)
+
+
+with open('kocici_fakta.json', mode='w', encoding='utf-8') as output_file:
+    json.dump(cat_facts, output_file, indent=4)
+
+
